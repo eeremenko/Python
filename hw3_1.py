1913c76aabdc4c5a128eb417c34534a192a3eb17
Evgeniy Eremenko <102290296+eeremenko@users.noreply.github.com>
2022-09-13 17:00:15 +0700

    Delete hw3_1.py

--- hw3_1.py
+++
@@ -1,9 +0,0 @@
-def my_del (x,y):
-    try:
-        return x / y
-    except ZeroDivisionError:
-        return('Деление на 0 запрещено')
-2
-a =  int(input('Введите число А: '))
-b = int(input('Введите число B: '))
-print(my_del(a, b))
